09899f9f3161924b57321b8fc996051432a49722
Putu Angga Risky Raharja <anggariskyraharja@gmail.com>
2020-12-28 20:42:09 +0700

    Android Final

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -16,7 +16,7 @@ FirebaseAnalytics.enable();
 })
 export class TabsPage implements OnInit, OnDestroy {
   options: AdOptions = {
-    adId: "ca-app-pub-9580735978410872/4749464888",
+    adId: "ca-app-pub-9580735978410872/1001791565",
     adSize: AdSize.BANNER,
     position: AdPosition.BOTTOM_CENTER,
   };
